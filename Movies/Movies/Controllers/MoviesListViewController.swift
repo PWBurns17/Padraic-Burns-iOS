09d0bd8a2a72aca4026c133108eba4e72924374a
PWBurns17 <89491370+PWBurns17@users.noreply.github.com>
2021-08-26 17:04:12 -0400

    Upload Movies application

---
+++ Movies/Movies/Controllers/MoviesListViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Christian Quicano on 23/08/21.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView!
+    private let viewModel = MovieViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupBinding()
+        
+    }
+    
+    private func setupBinding(){
+        // Create binding of movies
+        viewModel.movieBinding = { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        
+        viewModel.fetchMovies()
+    }
+}
+
+extension MoviesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieCell.identifier, for: indexPath) as? MovieCell
+        else { return UITableViewCell() }
+        
+        let row = indexPath.row
+        let title = viewModel.getTitle(at: row)
+        let overview = viewModel.getOverview(at: row)
+        cell.configureCell(title: title, overview: overview, imageData: nil)
+        
+        return cell
+    }
+    
+}
+
+extension MoviesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 98.0
+    }
+}
---
+++ Movies/Movies/View Models/MovieViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  MovieViewModel.swift
+//  Movies
+//
+//  Created by Field Employee on 24/08/2021.
+//
+
+import Foundation
+class MovieViewModel {
+    
+    // MARK:- internal properties
+    var movieBinding (() -> Void)?
+    var count: Int { movies.count }
+    
+    func getTitle (at row: Int) -> String { movies[row].originalTitle }
+    func getOverview (at row: Int) -> String { movies[row].overview }
+    
+    // MARK:- private properties
+    private let networkManager = NetworkManager()
+    private var movies = [Movie]()
+    
+    // MARK:- internal properties
+    private func fetchMovies() {
+        // create the url
+        let urlS = "https://api.themoviedb.org/3/movie/550?api_key=a53510055c2f6a1d5b665c62a0dccfcb&language=en-US"
+        
+        networkManager.getMovies(from: urlS) { [weak self] result in
+            switch result {
+            case .success(let movies):
+                self?.movies = movies
+                self?.movieBinding?()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
